2da415f7329b9e7e4938d72667a278aee10fb44a
Yoshiyuki Hirano <yhirano@me.com>
2021-12-16 21:48:01 +0900

    Implement StaticPages::PageComponent

--- app/components/static_pages/page_component.rb
+++ app/components/static_pages/page_component.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class StaticPages::PageComponent < ViewComponent::Base
+  def initialize(page:)
+    @page = page
+  end
 
+  private
+
+  attr_reader :page
 end
--- test/components/previews/static_pages/page_component_preview.rb
+++ test/components/previews/static_pages/page_component_preview.rb
@@ -1,5 +1,19 @@
 class StaticPages::PageComponentPreview < ViewComponent::Preview
   def default
-    render(StaticPages::PageComponent.new)
+    render(StaticPages::PageComponent.new(page: page))
+  end
+
+  private
+
+  def page
+    Page.new(id: 12345, content: FFaker::Lorem.paragraph, book: book)
+  end
+
+  def book
+    Book.new(id: 12345, title: FFaker::Lorem.sentence, author: author)
+  end
+
+  def author
+    Author.new(id: 12345, name: FFaker::Name.name)
   end
 end

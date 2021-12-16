308821c017a8ee769da8be51b92793b7b7fd520d
Yoshiyuki Hirano <yhirano@me.com>
2021-12-16 20:03:20 +0900

    Create authors table

--- db/migrate/20211216110155_create_authors.rb
+++ db/migrate/20211216110155_create_authors.rb
@@ -1,7 +1,7 @@
 class CreateAuthors < ActiveRecord::Migration[7.0]
   def change
-    create_table :authors do |t|
-      t.string :name
+    create_table :authors, comment: '著者' do |t|
+      t.string :name, null: false, comment: '名前'
 
       t.timestamps
     end
